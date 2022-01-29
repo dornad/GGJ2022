e352b52d5e2f615c45525e3d3769c23ec2cfa6a9
pauld <pauld@AW-PAULD-2018-8.drexel.edu>
2022-01-29 01:28:09 -0500

    got player to shoot projectiles. gonna work on explosion now

--- Assets/Scripts/FPSShooterTut.cs
+++ Assets/Scripts/FPSShooterTut.cs
@@ -3,17 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class FPSShooterTut : MonoBehaviour {
 
     public Camera cam;
     public GameObject projecile;
+    public GameObject muzzle;
     public Transform LHFirePoint, RHFirePoint;
+    public float projectileSpeed = 30.0f;
+    public float fireRate = 4.0f;
+    public float arcRange = 1.0f;
 
     private Vector3 destination;
     private bool leftHand;
-
-    
+    private float timeToFire;
 
     // Start is called before the first frame update
     void Start()
@@ -24,8 +28,9 @@ public class FPSShooterTut : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButtonDown("Fire1") && Time.time >= timeToFire)
         {
+            timeToFire = Time.time + 1 / fireRate;
             ShootProjectile();
         }
     }
@@ -54,5 +59,11 @@ public class FPSShooterTut : MonoBehaviour {
     void InstantiateProjectile(Transform firePoint)
     {
         var projectileObj = Instantiate(projecile, firePoint.position, Quaternion.identity) as GameObject;
+        projectileObj.GetComponent<Rigidbody>().velocity = (destination - firePoint.position).normalized * projectileSpeed;
+
+        iTween.PunchPosition(projectileObj, new Vector3(UnityEngine.Random.Range(-arcRange, arcRange), UnityEngine.Random.Range(-arcRange, arcRange), 0), UnityEngine.Random.Range(0.5f, 2.0f));
+
+        var muzzleObj = Instantiate(muzzle, firePoint.position, Quaternion.identity) as GameObject;
+        Destroy(muzzleObj, 2);
     }
 }
--- Assets/Scripts/ProjectileTut.cs
+++ Assets/Scripts/ProjectileTut.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileTut : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class ProjectileTut : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
+    public GameObject impactVFX;
+
+    private bool collided;
+
+    void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.tag != "Bullet" && collision.gameObject.tag != "Player" && !collided)
+        {
+            collided = true;
+
+            var impact = Instantiate(impactVFX, collision.contacts[0].point, Quaternion.identity) as GameObject;
+
+            Destroy(impact, 2);
+
+            Destroy(gameObject);
+
+        }
     }
 }
