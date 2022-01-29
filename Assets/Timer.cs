9df4cfcaeb77d3d66a563284b83d6135c8980ee8
Sargun Bindra <98240679+Sargun08@users.noreply.github.com>
2022-01-28 22:15:01 -0500

    got timer implemented

---
+++ Assets/Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Timer : MonoBehaviour
+{
+
+    public TextMeshProUGUI timerText;
+    private float startTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+}
+
