21e4076fc6ad7b8e28ecd0e48b94dd2aa68ceaba
rmello-dev <47431274+rmello-dev@users.noreply.github.com>
2022-01-30 14:18:41 -0500

    add plane triggers
    add elemental trigger to portal planes

---
+++ Assets/Scripts/Reactor_IcePortal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+
+public sealed class Reactor_IcePortal : MonoBehaviour
+    {
+    // --- Properties:
+
+    private readonly Abilities portalAbility = Abilities.Ice;
+
+    // --- External Behaviours:
+
+    public void OnCollisionEnter(Collision collisionParameters)
+        {
+        // if collides with player, set player ability to fire
+
+        ElementalProjectile.ChooseElement(portalAbility);
+        }
+
+
+    }
+
+
+}
